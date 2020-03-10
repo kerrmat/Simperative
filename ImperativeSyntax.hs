66bfd6d5fdbfef6c8dac293cce5e8b453d4a845e
Matthew Kerr <kerrmat@oregonstate.edu>
2020-03-10 15:23:57 -0700

    Implemented mutiple variable types

--- ImperativeSyntax.hs
+++ ImperativeSyntax.hs
@@ -4,7 +4,7 @@ module ImperativeSyntax where
 
 -- Abstract Syntax Document
 
-data Env = Vars [(String,Int)] [(String, Bool)] [(String, String)]
+data Env = Vars [(String,Int)] [(String, Bool)] [(String, String)] [(String, Function)]
             | Error -- Env Type contains Variable information / error
 
 data Program = Prog [Function] -- Program contains all syntax/code for any program
@@ -15,16 +15,16 @@ data Cmd = Assign String Expr  -- Can assign variables, run functions, or contro
             | If BoolExpr Cmd Cmd
             | While BoolExpr Cmd
             | List [Cmd]
-			| Exec String Env
+            | Exec String Env
             
 data Expr = Bexpr BoolExpr
-			| Iexpr IntExpr
-			| Sexpr StrExpr
+            | Iexpr IntExpr
+            | Sexpr StrExpr
              
 data BoolExpr = BVar String
                 | BConstant Bool  -- An expression that can be resloved to a bool value
                 | Compare CmpOperator IntExpr IntExpr
-				| IsEqual String String
+                | IsEqual String String
                 | Resolve BoolOperator BoolExpr BoolExpr
                 | Not BoolExpr
 
@@ -32,11 +32,11 @@ data IntExpr = IVar String  -- An expression that can be resolved to an int valu
                 | IConstant Int
                 | Simplify IntOperator IntExpr IntExpr
                 | Negative IntExpr
-				
+                
 data StrExpr = SVar String
-				| SConstant String
-				| Concat String String
-				
+                | SConstant String
+                | Concat String String
+                
                 
 
 
@@ -65,13 +65,16 @@ data IntOperator = Add
 -- Simple Print Function to show environment
 showEnv :: Env -> String
 showEnv Error = "Error"
-showEnv (Vars []) = ""
-showEnv (Vars ((s,e):xs)) = "(" ++ s ++ ", " ++ show e ++ ")" ++ showEnv(Vars xs)
+showEnv (Vars a b c d) = "Ints: \n" ++ showArray a ++ "\nBools: \n" ++ showArray b ++ "\nStrings: \n" ++ showArray c
+
+showArray :: [(String,b)] -> String
+showArray [] = ""
+showArray ((s,e):xs) = "(" ++ s ++ ", " ++ show e ++ ")" ++ showArray(xs)
 
 -- Can Be Errored by searching for nonexistent variable
-searchEnv :: Env -> String -> Bool
-searchEnv (Vars []) s = False
-searchEnv (Vars ((n,v):xs)) s = if s == n then True else searchEnv (Vars xs) s
+-- searchEnv :: Env -> String -> Bool
+-- searchEnv (Vars []) s = False
+-- searchEnv (Vars ((n,v):xs)) s = if s == n then True else searchEnv (Vars xs) s
 
 
 -- Function to map across environment - takes string and intexpr - if string = variable name, change variable to have new value
@@ -85,11 +88,11 @@ setVar newS newI (oldS,oldI) = if newS == oldS then (newS,newI) else (oldS,oldI)
   
 -- Good Program, sets x to 5, then if x=5, sets x to 1  
 testGoodProgram :: Program
-testGoodProgram = Prog [Func "Main" [Assign "x" (IConstant 5), If (Compare Equal (Var "x") (IConstant 5)) (Assign "x" (IConstant 1)) (Assign "x" (IConstant 10))]]
+testGoodProgram = Prog [Func "Main" [Assign "x" (IConstant 5), If (Compare Equal (IVar "x") (IConstant 5)) (Assign "x" (IConstant 1)) (Assign "x" (IConstant 10))]]
 
 -- Bad Program, sets x to 5, then if y=5, sets y to 5 - should return Error
 testBadProgram :: Program
-testBadProgram = Prog [Func "Main" [Assign "x" (IConstant 5), If (Compare Equal (Var "y") (IConstant 5)) (Assign "y" (IConstant 1)) (Assign "y" (IConstant 10))]]
+testBadProgram = Prog [Func "Main" [Assign "x" (IConstant 5), If (Compare Equal (IVar "y") (IConstant 5)) (Assign "y" (IConstant 1)) (Assign "y" (IConstant 10))]]
 
 testEnv :: Env
 testEnv = Vars []
@@ -120,34 +123,46 @@ function (Func n (x:xs)) e = function (Func n xs) (cmd x e)
 -- cmd should resolve if/while statements and assign variables
 cmd :: Cmd -> Env -> Env
 cmd c Error = Error
-cmd (Assign n i) (Vars is bs ss) = assignexpr n i e if (intexpr i (Vars e)) == Nothing
-									then Error
-									else if (searchEnv (Vars e) n) == Nothing
-										then let Just val = intexpr i (Vars e)
-											in Vars (e ++ [(n,val)])
-										else let Just val = intexpr i (Vars e)
-											in Vars (map (setVar n val) e)
+cmd (Assign n i) e = assignexpr n i e
 cmd (If b ca cb) e = if boolexpr b e == Nothing
                         then Error
                         else if boolexpr b e == Just True 
                             then cmd ca e
                             else cmd cb e
 cmd (While b c) e = e -- todo - implement while loops with a recursive function
-cmd (Exec String Env) e = 
+cmd (Exec fname params@(Vars ip bp sp fp)) (Vars is bs ss fs) = function (lookup fname fs) (Vars is++ip bs++bp ss++sp fs++fp)
 
 
-
-assignexpr :: Expr -> Env -> Env
-assignexpr (Iexpr e) (Vars is bs ss)  = if (intexpr i (Vars e)) == Nothing
-											then Error
-										else if (searchEnv (Vare e) n) == Nothing 
+-- Assigns a variable an expression value.  Returns environment with changed variable.
+assignexpr :: String -> Expr -> Env -> Env
+assignexpr s (Iexpr e) env@(Vars is bs ss f) = if (intexpr e env) == Nothing
+                                                then Error
+                                                else if (lookup s is) == Nothing 
+                                                    then let Just val = intexpr e env
+                                                        in (Vars (is ++ [(s,val)]) bs ss f)
+                                                    else let Just val = intexpr e env
+                                                        in (Vars (map (setVar s val) is) bs ss f)
+assignexpr s (Bexpr e) env@(Vars is bs ss f) = if (boolexpr e env) == Nothing
+                                                then Error
+                                                else if (lookup s bs) == Nothing 
+                                                    then let Just val = boolexpr e env
+                                                        in (Vars is (bs ++ [(s,val)]) ss)
+                                                    else let Just val = boolexpr e env
+                                                        in (Vars is (map (setVar s val) bs) ss f)
+assignexpr s (Sexpr e) env@(Vars is bs ss f) = if (strexpr e env) == Nothing
+                                                then Error
+                                                else if (lookup s ss) == Nothing 
+                                                    then let Just val = strexpr e env
+                                                        in (Vars is bs (ss ++ [(s,val)]))
+                                                    else let Just val = strexpr e env
+                                                        in (Vars is bs (map (setVar s val) ss) f)
 
 
 -- Turns an Integer Expression into an Integer
 intexpr :: IntExpr -> Env -> Maybe Int
 intexpr i Error = Nothing
 intexpr (IConstant x) e = Just x
-intexpr (Var s) e = searchEnv e s
+intexpr (IVar s) (Vars is bs ss fs) = lookup s is
 
 
 --Turns a Boolean Expression into a Boolean
